584b71fb68aec2fb2eeba567f11b130b0acb1ac4
Andrew Baxter <74902103+AndrewBaxter123@users.noreply.github.com>
2023-11-10 16:49:42 +0000

    minRating added to getMovieReviews file and working, implemented minRating with 1, same as no filter

--- lambdas/getMovieReviews.ts
+++ lambdas/getMovieReviews.ts
@@ -1,7 +1,7 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
-import { validateYear } from "../shared/util";
+import { validateRating, validateYear } from "../shared/util";
 
 const ddbClient = new DynamoDBClient({ region: process.env.REGION });
 const ddbDocClient = DynamoDBDocumentClient.from(ddbClient);
@@ -11,44 +11,50 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
     if (!event.pathParameters || !event.pathParameters.movieId) {
       return {
         statusCode: 400,
-        headers: {
-          "content-type": "application/json",
-        },
+        headers: { "content-type": "application/json" },
         body: JSON.stringify({ message: "Missing movieId in path parameters" }),
       };
     }
 
     const movieId = parseInt(event.pathParameters.movieId);
-
     if (isNaN(movieId)) {
       return {
         statusCode: 400,
-        headers: {
-          "content-type": "application/json",
-        },
+        headers: { "content-type": "application/json" },
         body: JSON.stringify({ message: "movieId must be a valid number" }),
       };
     }
 
-    // validate the year if present
+    // Validate the year if present
     const year = event.queryStringParameters?.year;
     if (year && !validateYear(year)) {
       return {
         statusCode: 400,
-        headers: {
-          "content-type": "application/json",
-        },
+        headers: { "content-type": "application/json" },
         body: JSON.stringify({ message: "Invalid year format. Expected format: YYYY" }),
       };
     }
 
-    let keyConditionExpression = 'movieId = :movieId';
-    let expressionAttributeValues = {
-      ':movieId': movieId,
-    };
-    let indexName: string | undefined = undefined //will only use this when year is provided ?year
+    // validate minRating if present
+    const minRatingParam = event.queryStringParameters?.minRating;
+    let minRating = 1
+    if (minRatingParam !== undefined) {
+      const parsedRating = parseInt(minRatingParam, 10);
+      if (isNaN(parsedRating) || !validateRating(parsedRating)) {
+          return {
+              statusCode: 400,
+              headers: { "content-type": "application/json" },
+              body: JSON.stringify({ message: "Invalid minRating value. It must be a number between 1 and 10." }),
+          };
+      }
+      minRating = parsedRating;
+  }
+  
 
-    // Modify query if year is provided  -?year
+    let keyConditionExpression = 'movieId = :movieId';
+    let expressionAttributeValues = { ':movieId': movieId };
+    let indexName: string | undefined = undefined;
+
     if (year) {
       keyConditionExpression += ' and begins_with(reviewDate, :year)';
       expressionAttributeValues[':year'] = year;
@@ -64,36 +70,30 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
       })
     );
 
-    if (!commandOutput.Items) {
+    let items = commandOutput.Items || [];
+
+    // Filter reviews by minRating if provided
+    items = items.filter(item => item.rating >= minRating);
+
+    if (items.length === 0) {
       return {
         statusCode: 404,
-        headers: {
-          "content-type": "application/json",
-        },
+        headers: { "content-type": "application/json" },
         body: JSON.stringify({ message: "No reviews found for this movie" }),
       };
     }
 
-    const body = {
-      data: commandOutput.Items,
-    };
-
     return {
       statusCode: 200,
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(body),
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ data: items }),
     };
   } catch (error: any) {
     console.log(JSON.stringify(error));
     return {
       statusCode: 500,
-      headers: {
-        "content-type": "application/json",
-      },
+      headers: { "content-type": "application/json" },
       body: JSON.stringify({ error }),
     };
   }
 };
-
